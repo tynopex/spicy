07a5db278df562666abfaccf81ee8158e97118e4
Ted <ted@weaksignal.tynopex.net>
2015-07-01 22:46:24 -0400

    Add simple parser

---
+++ src/fileformat.rs
@@ -0,0 +1,90 @@
+/*
+    # Sample.spicy
+    CV[3.3] 0 1
+    R[10k] 0 1
+    R[1k] 1 2
+    R[4700] 0 2
+    CC[10m] 0 2
+*/
+
+use std::fs::File;
+use std::io::BufReader;
+use std::io::BufRead;
+
+use super::{Graph,ElementType,Element,Net};
+
+
+fn load_spicy(fname: &str) -> Option<Graph>
+{
+    let f = File::open(fname).unwrap();
+
+    let mut gr = Graph { nodes: vec![] };
+
+    for line in BufReader::new(f).lines()
+    {
+        let line = line.unwrap();
+        let line = line.trim();
+
+        if line.starts_with("#")
+        {
+            continue;
+        }
+
+        let parts: Vec<&str> = line.split_whitespace().collect();
+        assert!(parts.len() == 4);
+
+        let kind = match parts[0] {
+            "R" => ElementType::Resistor,
+            "CV" => ElementType::ConstantVoltage,
+            "CC" => ElementType::ConstantCurrent,
+            _ => panic!("Unknown type"),
+            };
+
+        let val = parse_value(parts[1]);
+
+        let net_a: Net = parts[2].parse().unwrap();
+        let net_b: Net = parts[3].parse().unwrap();
+
+        let el = Element { kind: kind, value: val, nets: (net_a,net_b) };
+
+        gr.nodes.push(el);
+    }
+    
+    None
+}
+
+fn parse_value(s: &str) -> f32
+{
+    let idx = s.rfind(char::is_alphabetic);
+
+    let mut val: f32 = match idx {
+        Some(i) => &s[..i],
+        None => s,
+        }
+        .parse()
+        .unwrap();
+
+    if idx.is_some()
+    {
+        let i = idx.unwrap();
+        let scale: f32 = match &s[i..] {
+            "p" => 1e-12,
+            "n" => 1e-9,
+            "u" => 1e-6,
+            "m" => 1e-3,
+            "k" => 1e3,
+            "M" => 1e6,
+            _ => panic!("Unknown size prefix"),
+            };
+
+        val *= scale;
+    }
+
+    val
+}
+
+#[test]
+fn test_load()
+{
+    load_spicy("sample.spicy");
+}
--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,28 @@
-#[test]
-fn it_works() {
+#![allow(dead_code)]
+
+mod fileformat;
+
+
+type Net = u32;
+type Nets = (Net,Net);
+
+#[derive(Debug)]
+enum ElementType
+{
+    Resistor,
+    ConstantVoltage,
+    ConstantCurrent,
+}
+
+#[derive(Debug)]
+struct Element
+{
+    kind: ElementType,
+    value: f32,
+    nets: Nets,
+}
+
+struct Graph
+{
+    nodes: Vec<Element>,
 }
